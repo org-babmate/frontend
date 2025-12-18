7c10c8e3c50e9f587b0e8eb25a6bb0a50e55b39c
Carl Lee <ckdwkdl91@gmail.com>
2025-12-18 09:58:10 +0900

    🎨 Fix: fix global header

--- src/shared/ui/header/index.tsx
+++ src/shared/ui/header/index.tsx
@@ -1,15 +1,22 @@
 import CustomSheet from '@/widget/sheet';
 import Link from 'next/link';
 
-function Header() {
+interface HeaderProps {
+  withSignIn?: boolean;
+  title?: string;
+}
+
+function Header({ withSignIn = true, title = 'Babmate' }: HeaderProps) {
   return (
     <header className="absolute top-0 left-0 right-0 z-50 bg-transparent w-full h-[52px]">
       <div className="flex w-full pt-5 pb-2 items-center justify-between">
-        <h1 className="text-lg font-semibold">Babmate</h1>
+        <h1 className="text-lg font-semibold">{title}</h1>
         <div className="flex flex-row items-center gap-5">
-          <Link href="/login" className="text-sm font-medium">
-            Sign In
-          </Link>
+          {withSignIn && (
+            <Link href="/login" className="text-sm font-medium">
+              Sign In
+            </Link>
+          )}
           <CustomSheet />
         </div>
       </div>
