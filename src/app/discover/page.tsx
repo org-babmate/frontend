19a41ed0320f7bc05b5d4e071915c539b4ef8337
robin <nowrobin3@gmail.com>
2026-01-09 14:46:50 +0900

    fix: calendar height

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -15,16 +15,16 @@ export default function DiscoverPage() {
   const toStr = sp.get('to');
 
   return (
-    <div className="flex flex-col items-center  min-h-screen w-full">
-      <Header />
-      <div className="w-full border-b py-3 px-4 mt-13">
-        <Suspense fallback={null}>
+    <Suspense fallback={null}>
+      <div className="flex flex-col items-center  min-h-screen w-full">
+        <Header />
+        <div className="w-full border-b py-3 px-4 mt-13">
           <FilterBar filters={filterState} onFilterChange={setFilterState} />
-        </Suspense>
+        </div>
+        <div className="flex flex-col items-center gap-4 w-full p-4">
+          <ExperienceList searchParams={apiParams} />
+        </div>
       </div>
-      <div className="flex flex-col items-center gap-4 w-full p-4">
-        <ExperienceList searchParams={apiParams} />
-      </div>
-    </div>
+    </Suspense>
   );
 }
